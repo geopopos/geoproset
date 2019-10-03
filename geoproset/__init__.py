014dfde06212076687c4d6f180dbfa45524a280f
Geopopos <groros2@icloud.com>
2019-10-03 11:38:19 -0400

    updated geoproset to modfiy config.json with project info

--- geoproset/__init__.py
+++ geoproset/__init__.py
@@ -1,17 +1,18 @@
-import click, os, shutil
+import click, os, shutil, json
 
 @click.command()
-@click.option('-d', '--directory')
+@click.option('-l', '--location')
 @click.option('-r', '--root')
+@click.option('-d', '--description')
 @click.argument('project_name')
-def main(directory, root, project_name):
+def main(location, root, description, project_name):
     # Make copy of skeleton project directory to location of new project
     click.echo('Creating new project: {}'.format(project_name))
-    if not directory:
-        directory = './'
+    if not location:
+        location = './'
     module_directory = os.path.abspath(os.path.dirname(__file__))
     skeleton_directory = os.path.join(module_directory, 'skeleton/')
-    project_directory = '{}{}/'.format(directory, project_name)
+    project_directory = '{}{}/'.format(location, project_name)
     shutil.copytree(skeleton_directory, project_directory)
     # Rename the root module
     initial_module = '{}NAME/'.format(project_directory)
@@ -31,3 +32,14 @@ def main(directory, root, project_name):
         module_test = '{}tests/{}_test.py'.format(project_directory, project_name)
         click.echo('Renaming {} to {}'.format(initial_test, module_test))
     os.rename(initial_test, module_test)
+    # Update config.json
+    click.echo("Updating config file...")
+    config_file = '{}config.json'.format(project_directory)
+    with open(config_file, 'r') as read_file:
+        config = json.load(read_file)
+    config["description"] = description or project_name
+    config["name"] = project_name
+    config["packages"].remove("NAME")
+    config["packages"].append(project_name)
+    with open(config_file, 'w') as write_file:
+        json.dump(config, write_file)
